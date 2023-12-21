3c70aa1bf30c99b887819b3d1c99e35f657de8ed
Amanda <amanda.ausejo@gmail.com>
2023-12-21 17:18:26 +0100

    fix Milestone 8

--- src/PersonPage.jsx
+++ src/PersonPage.jsx
@@ -1,18 +1,44 @@
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
+import { useContext } from "react";
 const apiKey = import.meta.env.VITE_API_KEY;
+import LanguageContext from "./LanguageContext";
 
 const PersonPage = () => {
     const { id } = useParams();
-
     const [error, setError] = useState("");
     const [actorById, setActorById] = useState();
+
+    const lang = useContext(LanguageContext);
+
+    const ChangeLanguagePerPage = {
+        "en-US": {
+            name: "Name",
+            gender: "Gender",
+            age: "Age",
+            occupation: " Occupation",
+            bio: "Bio",
+            male: "male",
+            female: "female",
+            title: "Actor/Actress id:",
+        },
+        "it-IT": {
+            name: "Nome",
+            gender: "Sesso",
+            age: "Eta",
+            occupation: " Occupazione",
+            bio: "Biografia",
+            male: "maschio",
+            female: "femmina",
+            title: "Attore/Attrice id:",
+        },
+    };
     useEffect(() => {
         const fetchActorsById = async () => {
             try {
                 const response = await fetch(
-                    `https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}`
+                    `https://api.themoviedb.org/3/person/${id}?api_key=${apiKey}&language=${lang}`
                 );
                 const obj = await response.json();
                 if (obj.success === false) {
@@ -26,13 +52,15 @@ const PersonPage = () => {
             }
         };
         fetchActorsById();
-    }, [id]);
+    }, [id, lang]);
 
     return (
         <>
             {!error && actorById ? (
                 <div className="id-actor-container">
-                    <h1>Actor/Actress id: {id}</h1>
+                    <h1>
+                        {ChangeLanguagePerPage[lang].title} {id}
+                    </h1>
 
                     <figure>
                         <img
@@ -42,22 +70,31 @@ const PersonPage = () => {
                     </figure>
                     <div className="info">
                         <p>
-                            <strong>Name:</strong>
+                            <strong>
+                                {" "}
+                                {ChangeLanguagePerPage[lang].name}:{" "}
+                            </strong>
                             {actorById.name}
                         </p>
                         <p>
-                            <strong>Gender:</strong>
+                            <strong>
+                                {" "}
+                                {ChangeLanguagePerPage[lang].gender}:
+                            </strong>
                             {actorById.gender === 1 ? "female" : "male"}
                         </p>
                         <p>
-                            <strong>Age:</strong>
+                            <strong> {ChangeLanguagePerPage[lang].age}:</strong>
                         </p>
                         <p>
-                            <strong>Profession:</strong>
+                            <strong>
+                                {" "}
+                                {ChangeLanguagePerPage[lang].occupation}:
+                            </strong>
                             {actorById.known_for_department}
                         </p>
                         <p className="bio">
-                            <strong>Bio:</strong>
+                            <strong> {ChangeLanguagePerPage[lang].bio}:</strong>
                             {actorById.biography}
                         </p>
                     </div>
